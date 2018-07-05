06ea4e2557eaa2409f3d0d546569680cb696a15d
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-07-05 11:52:48 -0300

    correccion

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,9 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var nombre;
+	var edad;
+	nombre= prompt("ingrese su nombre");
+	edad= prompt("ingrese su edad");
+	alert("Usted se llama "+nombre+" y tiene "+ edad +" años");
 }
-
