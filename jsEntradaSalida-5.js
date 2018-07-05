223dc90c05cfc206565589ecdd566171cc91f3d4
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-07-05 11:42:44 -0300

    ejercicio 5

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+	var nombre;
+	var edad;
+	var nombre=" gabriela";
+	var edad="20";
+	alert("Usted se llama "+nombre+" y tiene "+ edad +" años");
+}
+
