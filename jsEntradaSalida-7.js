9763def60f2b047310dc3ac077c57823750e8235
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-07-06 12:04:45 -0300

    6/7

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,68 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno+numeroDos;
+	alert("El resultadode la suma es: "+resultado)
+}
+
+function restar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno- numeroDos;
+	alert("El resultado de la resta es: "+resultado)
+}
+
+function multiplicar()
+{ 
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+    numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno*numeroDos;
+	alert("El resultado de la multiplicación es: "+resultado)
+}
+
+function dividir()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno/numeroDos;
+	alert("El resultado de la división es: "+resultado)
+}
+
