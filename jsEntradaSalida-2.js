0a6f57123517243d1cd6af2c70cf737f1840a7a1
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-07-07 10:10:38 -0300

    s

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var nombre;
+	//var nombre=" çgabriela";
+	//alert(nombre);//
+	nombre= prompt("ingrese su nombre");
+	alert("Su nombre es:"+nombre);
+	//var person = prompt("Please enter your name", "Harry Potter");
+
+}
+
+/*bloque de comentario o apunte, se hace para comentar algoy siempre setiene que cerrar el bloque*/
+//linea que no sale en panatalla//
+/* esto lo saque de W3school*/
