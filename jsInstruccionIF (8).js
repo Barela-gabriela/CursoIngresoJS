3b2db3c63bc7cdc866948d5b1aeb4d444c25e6fb
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-07-12 11:50:43 -0300

    ej 8 if

---
+++ jsInstruccionIF (8).js
@@ -0,0 +1,21 @@
+function mostrar()
+{
+	var edad;
+	var EstadoCivil;
+	edad= document.getElementById('edad').value;
+	EstadoCivil=document.getElementById('estadoCivil').value;
+	if(edad<18 != "Soltero")
+	{
+		alert("")
+	}
+	if(edad>18 || "Soltero")
+	{
+		alert("Es soltero y no es menor de edad")
+	}
+
+
+
+	
+
+
+}//FIN DE LA FUNCIÓN
