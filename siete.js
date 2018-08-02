810c92a565ecdbfc87c319b17e3ea1243a7dd1a8
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-08-02 12:22:29 -0300

    fidiouyy0fg

---
+++ siete.js
@@ -0,0 +1,32 @@
+function mostrar()
+{
+	var edad;
+	var sexo;
+	var nombre;
+	var nota;
+	var contador;
+	var peorNota;
+	var peorNotaSexo;
+	var masViejo;
+	var masViejoNombre;
+
+	contador=0;
+
+	while(contador<6)
+	{
+		contador++;
+		nombre= prompt("Ingrese nombre");
+
+		sexo= prompt("Ingrese sexo");
+		while(sexo!="f"&& sexo!="m")
+		{
+			sexo= prompt("Ingrese sexo");
+		}
+
+		edad=prompt("Ingrese edad");
+
+		edad=parseInt(edad);
+		while()
+	}
+
+}
