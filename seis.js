4bb2dbd7928cc5924de9b2df36a80a6540c1dbf1
Barela-gabriela <40859238+Barela-gabriela@users.noreply.github.com>
2018-08-11 11:31:44 -0300

    Add files via upload

--- seis.js
+++ seis.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
-	alert("comentar esta linea 6");
+	var estacionDelAño;
+	var temperatura;
+
+	estacionDelAño=prompt("Ingrese la estacion del año");
+	temperatura=prompt("Ingrese la temperatura");
+
+	temperatura=parseInt(temperatura);
+
+	switch(estacionDelAño)
+	{
+		case: "Otoño"
+		if(temperatura==15)
+			{
+				alert("Esta lindo.");
+			}else
+			{
+				if (temperatura<15)
+				{
+					alert("Se vino el frio.");
+				}else
+				{
+					if(temperatura>15 && temperatura<=21){
+						alert("Esta re bueno!!");
+					}else
+					{
+						alert("Mucho calor para Otoño");
+					}
+				}
+			}
+	}
+
 }
